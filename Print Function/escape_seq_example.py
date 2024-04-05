adedacc24074f9d3ae2263ce2739ca49a59b5b22
Sameer <kalantrilonda@gmail.com>
2024-04-05 22:24:50 +0530

    escape sequence example completed

---
+++ Print Function/escape_seq_example.py
@@ -0,0 +1,14 @@
+# # print("It is popularly said -"No pain no gain"")
+ 
+# print("It is popularly said -\"No pain no gain\"") 
+# # escape sequence 
+# # \n new line 
+# # \t tab standard 4 spaces 
+# # \\ backslash
+# # \' single quote
+# # \" double quote
+# # \b backspace
+# # \r carriage return
+
+# print("Rochak education \n is a great institute")
+print("Rochak education is a great\r institute")
